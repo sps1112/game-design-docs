59a175d0e2fb7b9275e2ea3850cb27d4cba3dde3
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-06-15 17:07:03 +0530

    Update extensions in conf.py

--- conf.py
+++ conf.py
@@ -48,6 +48,7 @@ release = 'V 1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
